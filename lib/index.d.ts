5bcaba25c37ae170235a784819506159f0f57b6c
Ilfat Galiev <v-ilgali@microsoft.com>
2018-06-06 10:22:11 +0300

    Export testDataViewBuilder

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,7 @@
-import { testDom, createContextMenuEvent, createMouseEvent, createTouch, createTouchesList, createTouchEndEvent, createTouchMoveEvent, createTouchStartEvent } from "./helpers/helpers";
+import { testDom, createContextMenuEvent, createMouseEvent, createTouch, createTouchesList, createTouchEndEvent, createTouchMoveEvent, createTouchStartEvent, getRandomNumber, getRandomNumbers } from "./helpers/helpers";
 import { createVisualHost, createSelectionId, createAllowInteractions, createColorPalette, createLocale, createSelectionIdBuilder, createSelectionManager, createTooltipService } from "./mocks/mocks";
 import { setGrouped, createValueColumns, createCategoricalDataViewBuilder } from "./dataViewBuilder/dataViewBuilder";
+import * as testDataViewBuilder from "./dataViewBuilder/testDataViewBuilder";
 import * as visualBuilderBase from "./VisualBuilderBase";
 import VisualBuilderBase = visualBuilderBase.VisualBuilderBase;
-export { testDom, createContextMenuEvent, createMouseEvent, createTouch, createTouchesList, createTouchEndEvent, createTouchMoveEvent, createTouchStartEvent, createVisualHost, createSelectionId, createAllowInteractions, createColorPalette, createLocale, createSelectionIdBuilder, createSelectionManager, createTooltipService, setGrouped, createValueColumns, createCategoricalDataViewBuilder, VisualBuilderBase };
+export { testDom, createContextMenuEvent, createMouseEvent, createTouch, createTouchesList, createTouchEndEvent, createTouchMoveEvent, createTouchStartEvent, createVisualHost, createSelectionId, createAllowInteractions, createColorPalette, createLocale, createSelectionIdBuilder, createSelectionManager, createTooltipService, setGrouped, createValueColumns, createCategoricalDataViewBuilder, VisualBuilderBase, testDataViewBuilder, getRandomNumber, getRandomNumbers };
